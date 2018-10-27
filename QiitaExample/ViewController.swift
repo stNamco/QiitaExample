a3f1627933d2dd293709a081a3a20d70cc8721b2
stNamco <lbnamco15@gmail.com>
2018-10-27 21:02:29 +0900

    add project

---
+++ QiitaExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QiitaExample
+//
+//  Created by stnamco on 2018/10/27.
+//  Copyright © 2018年 namco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
