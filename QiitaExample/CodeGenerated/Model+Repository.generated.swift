cb353ce44a52f11cccabffab85ce4ae9aec24ed3
stNamco <lbnamco15@gmail.com>
2018-10-27 21:34:51 +0900

    add templates

---
+++ QiitaExample/CodeGenerated/Model+Repository.generated.swift
@@ -0,0 +1,6 @@
+// Generated using Sourcery 0.13.1 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+import Foundation
+import Moya
+
--- Scripts/generate-maincode-using-sourcery.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-# sourcery - generate main target code.
-Pods/Sourcery/bin/sourcery --sources "SourceryExamples" --templates "SourceryExamples/Sources/Templates" --output "SourceryExamples/Sources/CodeGenerated"
