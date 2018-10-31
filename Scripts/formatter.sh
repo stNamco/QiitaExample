c5566f218a5ffc88b0f1569a952fa8f90e426946
stNamco <lbnamco15@gmail.com>
2018-10-31 20:12:46 +0900

    added generated rule to swiftformat

--- Scripts/formatter.sh
+++ Scripts/formatter.sh
@@ -4,4 +4,4 @@
 ./Scripts/sort-Xcode-project-file *.xcodeproj
 
 # swiftformat
-Pods/SwiftFormat/CommandLineTool/swiftformat . --exclude Carthage,Pods --trimwhitespace nonblank-lines --stripunusedargs closure-only --patternlet inline  --disable strongOutlets,trailingCommas
+Pods/SwiftFormat/CommandLineTool/swiftformat . --exclude Carthage,Pods --allman false --wraparguments beforefirst --wrapelements beforefirst --self remove --header ignore --binarygrouping none --octalgrouping none --indentcase false --trimwhitespace always --decimalgrouping none --patternlet inline --commas inline --semicolons inline --indent 4 --exponentcase lowercase --operatorfunc spaced --elseposition same-line --empty void --ranges spaced --hexliteralcase uppercase --linebreaks lf --hexgrouping none --comments indent --ifdef indent --stripunusedargs closure-only
